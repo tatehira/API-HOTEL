eb1429cd2c15b7f4067cfbc1ceebfeb33edd4c34
Tatehira <niickcostta00@outlook.com>
2023-08-25 01:12:47 -0400

    Update ignore e list

--- ProductsApi/Models/HotelModels/Hotel.cs
+++ ProductsApi/Models/HotelModels/Hotel.cs
@@ -6,8 +6,9 @@ namespace ProductsApi.Models.HotelModels
     public class Hotel
     {
         public int Id { get; set; }
+        public double ValorDiaria { get; set; }
         public string NomeHotel { get; set; }
-        public Quarto Quarto { get; set; }
+        public List<Quarto> Quarto { get; set; }
         public Regiao Regiao { get; set; }
     }
 
